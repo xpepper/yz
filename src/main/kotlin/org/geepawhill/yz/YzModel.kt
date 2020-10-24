5f918358c0ec8c6e1ee3e00fcaf1ad7898407b53
Pietro Di Bello <pierodibello@gmail.com>
2020-10-24 12:26:19 +0200

    rearrange methods order

--- src/main/kotlin/org/geepawhill/yz/YzModel.kt
+++ src/main/kotlin/org/geepawhill/yz/YzModel.kt
@@ -1,23 +1,26 @@
 package org.geepawhill.yz
 
 import javafx.beans.binding.Bindings.greaterThan
+import javafx.beans.binding.BooleanBinding
 import javafx.beans.property.SimpleIntegerProperty
-import java.lang.RuntimeException
 import java.util.*
 
 class YzModel {
     private val random = Random()
     private val rollsLeftProperty = SimpleIntegerProperty(0)
 
-    val canRoll = greaterThan(rollsLeftProperty, 0)
-    
+    val canRoll: BooleanBinding = greaterThan(rollsLeftProperty, 0)
+
     val dice = listOf(
-            SimpleIntegerProperty(0),
             SimpleIntegerProperty(0),
             SimpleIntegerProperty(0),
             SimpleIntegerProperty(0),
             SimpleIntegerProperty(0)
     )
+
+    fun start() {
+        rollsLeftProperty.value = 3
+    }
 
     fun roll() {
         if (!canRoll.value) throw RuntimeException("Illegal roll called!")
@@ -25,9 +28,5 @@ class YzModel {
             die.value = random.nextInt(6) + 1
         }
         rollsLeftProperty.value--
-    }
-
-    fun start() {
-        rollsLeftProperty.value = 3
     }
 }
